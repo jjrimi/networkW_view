616d5e45a58225f1347f763e2d0a3c8d73bdebba
jjrimi <145416888+jjrimi@users.noreply.github.com>
2023-12-06 14:50:43 +0900

    mystie

---
+++ setytings.py
@@ -0,0 +1,24 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+},
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'logs' / 'mysite.log',
+            'formatter': 'verbose'
+},
+    },
+    'loggers': {
+        'polls': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
